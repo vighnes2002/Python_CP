73c930e68ff95384d796119b5a0767127e28fdc4
Varun Nayak <68057853+varun406@users.noreply.github.com>
2022-10-22 22:15:15 +0530

    Update AplusB.java

--- AplusB.java
+++ AplusB.java
@@ -5,6 +5,6 @@ public class AplusB {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.print(a + b);
+        System.out.println(a + b);
     }
 }
