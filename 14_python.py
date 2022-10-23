35b5aae8ed5d8ba83c9a9f5a7641593dea39fcfb
Varun Nayak <68057853+varun406@users.noreply.github.com>
2022-10-23 11:22:56 +0530

    Update 14_python.py

--- 14_python.py
+++ 14_python.py
@@ -1,17 +1,15 @@
 num = int(input("Enter a number: "))
 flag = False
 
-# prime numbers are greater than 1
 if num > 1:
-    # check for factors
+ 
     for i in range(2, num):
         if (num % i) == 0:
-            # if factor is found, set flag to True
+            
             flag = True
-            # break out of loop
+           
             break
 
-# check if flag is True
 if flag:
     print(num, "is not a prime number")
 else:
